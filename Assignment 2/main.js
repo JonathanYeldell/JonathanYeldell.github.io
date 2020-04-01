1405e4e76ca7f5e8feed0358e5b9d6ba51834981
Jonathan Yeldell <yeldelljon@gmail.com>
2020-04-01 18:47:18 -0400

    Update main.js

--- Assignment 2/main.js
+++ Assignment 2/main.js
@@ -5,6 +5,14 @@ const btn = document.querySelector('button');
 const overlay = document.querySelector('.overlay');
 
 /* Looping through images */
+var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+
 
 const newImage = document.createElement('img');
 newImage.setAttribute('src', xxx);
